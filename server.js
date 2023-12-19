c16bfc80a30c7b79c237a6da6ff9e431c8679e79
Glitch (glitch-hello-node) <none>
2023-12-19 04:00:06 +0000

    📖🐜 Checkpoint ./server.js:63216303/122

--- server.js
+++ server.js
@@ -184,45 +184,45 @@ app.post("/recipes", (req, res) => {
   console.log(req.body.name_food);
   console.log(req.body.time);
   console.log(req.body);
-//   imgRecipesUploadMiddleware(req, res, async (err) => {
-//     const ingredients = req.files.img_ingredients.map((file, index) => {
-//       const link = "/upload/" + file.filename;
-//       const url = API_URL + link;
-//       return {
-//         name: req.body[`name${index}`],
-//         quality: req.body[`quality${index}`],
-//         img_url: url,
-//       }
-//     });
+  imgRecipesUploadMiddleware(req, res, async (err) => {
+    const ingredients = req.files.img_ingredients.map((file, index) => {
+      const link = "/upload/" + file.filename;
+      const url = API_URL + link;
+      return {
+        name: req.body[`name${index}`],
+        quality: req.body[`quality${index}`],
+        img_url: url,
+      }
+    });
                                                       
-//     const steps = req.files.img_making.map((file, index) => {
-//       const link = "/upload/" + file.filename;
-//       const url = API_URL + link;
-//       return {
-//         id: req.body[`step${index}`],
-//         making: req.body[`making${index}`],
-//         img_url: url,
-//       }
-//     });
+    const steps = req.files.img_making.map((file, index) => {
+      const link = "/upload/" + file.filename;
+      const url = API_URL + link;
+      return {
+        id: req.body[`step${index}`],
+        making: req.body[`making${index}`],
+        img_url: url,
+      }
+    });
     
-//     const data = {
-//       _id_user: req.body._id_user,
-//       name_food: req.body.name_food,
-//       img_url: req.body.img_url,
-//       video_url: req.body.video_url,
-//       time: req.body.time,
-//       ingredient: ingredients,
-//       step: steps,
-//       topics: req.body.topics,
-//     };
+    const data = {
+      _id_user: req.body._id_user,
+      name_food: req.body.name_food,
+      img_url: req.body.img_url,
+      video_url: req.body.video_url,
+      time: req.body.time,
+      ingredient: ingredients,
+      step: steps,
+      topics: req.body.topics,
+    };
 
-//     const newRecipe = await new recipes(data).save();
-//     await user.findByIdAndUpdate(
-//       req.body._id_user,
-//       { $push: { recipes: newRecipe._id }}
-//     );
-//     res.status(200).send({ message: 'Thêm công thức thành công' });
-//   });
+    const newRecipe = await new recipes(data).save();
+    await user.findByIdAndUpdate(
+      req.body._id_user,
+      { $push: { recipes: newRecipe._id }}
+    );
+    res.status(200).send({ message: 'Thêm công thức thành công' });
+  });
 });
 
 app.listen(port, () =>
