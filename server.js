a3f4e1f9dec87017d13942f0dd2b032bebe42e75
Glitch (glitch-hello-node) <none>
2024-01-11 16:41:15 +0000

    🛤🏭 Checkpoint ./server.js:63216303/13

--- server.js
+++ server.js
@@ -362,7 +362,7 @@ app.delete("/post/:id", async (req, res) => {
 
 app.post("/recipes", (req, res) => {
   imgRecipesUploadMiddleware(req, res, async (err) => {
-    console.log(req.body);
+    console.log(req.body.img.filename);
     console.log(req.files);
     const ingredients = req.files.img_ingredients.map((file, index) => {
       const link = "/upload/" + file.filename;
